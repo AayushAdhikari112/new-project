af6f0e32920fe787a172b98f5e0b86c9ce6b286f
Aayush11 <aawaj1660@gmail.com>
2025-10-19 20:02:52 +0545

    my first py project

---
+++ var_prac.py
@@ -0,0 +1,11 @@
+user_name="Groot"
+print(f"your username is {user_name}")
+
+year = 2025
+print(f"this year is {year}")
+
+pi=3.14
+print(f"the value of pie is known to be {pi}")
+
+is_admin= True
+print(f" are you a admin ?: {is_admin}")
